0a1d499f3c93a9067a8e060014f127f346d7a489
Kadir <kyurtsever.mail@gmail.com>
2022-09-04 11:55:15 +0200

    HttpClient and AuthorizationMessageHandller created.

---
+++ src/OrderPlanner/AuthorizationMessageHandler.cs
@@ -0,0 +1,22 @@
+﻿using Blazored.LocalStorage;
+
+public class AuthorizationMessageHandler : DelegatingHandler
+{
+    ILocalStorageService _storageService;
+    public AuthorizationMessageHandler(ILocalStorageService storageService)
+    {
+        _storageService = storageService;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        if(await _storageService.ContainKeyAsync("access_token"))
+        {
+            var token = await _storageService.GetItemAsStringAsync("access_token");
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+        }
+
+        Console.WriteLine("Authorization Message Handler was called.");
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
--- src/OrderPlanner/Program.cs
+++ src/OrderPlanner/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -7,8 +8,15 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("OrderPlanner.Api", client =>
+{
+    client.BaseAddress = new Uri("https://abc.com");
+}).AddHttpMessageHandler<AuthorizationMessageHandler>();
 
+builder.Services.AddTransient<AuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("OrderPlanner.Api"));
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
